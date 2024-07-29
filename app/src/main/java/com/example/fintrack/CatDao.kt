8eb1a00a017482c90ae332d65687346564b1eeaf
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2024-07-29 13:56:22 -0300

    //Sétimo dia: depois de procrastinar por 4 dias (vergonha hein?) eu consegui implementar apenas uma entidade e criar o banco de dados do aplicativo. Mas não o declarei nem inicializei na main activity ainda, pq tava revisando as aulas.

--- app/src/main/java/com/example/fintrack/CatDao.kt
+++ app/src/main/java/com/example/fintrack/CatDao.kt
@@ -3,7 +3,7 @@ package com.example.fintrack
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-
+//comit
 interface CatDao {
     @Query("Select * From catentity")
     fun getAll(): List<CatEntity>
